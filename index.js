938ebc790fab9c418929669a3e1d98a8ee00c253
KNMR-GITHUB <64697482+KNMR-GITHUB@users.noreply.github.com>
2022-09-16 22:45:41 +0530

    updated

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 rating = 0;
 
 $(".circle").click(function(e){
+    $("#"+rating).removeClass("backgroundChange");
     console.log(e.target.id);
+    console.log()
     rating = e.target.id;
+    $("#"+rating).addClass("backgroundChange");
     localStorage.setItem("rating",rating);
 });
